0e208b3e46ef6dcc905813a36e41e9fc676ff212
Chinecherem Harry <chinecherempharry@gmail.com>
2024-03-26 13:00:40 +0100

    added spaces to partners section

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -4,14 +4,14 @@ import { GoDash } from 'react-icons/go'
 
 const Partners = () => {
     return (
-        <section id="partner" className="my-20 container h-full w-full mx-auto ">
+        <section id="partner" className="my-20 contentContainer h-full w-full mx-auto ">
             <div className="flex flex-col">
                 <span className='flex items-center '>
                     {/* <GoDash size={70} /> */}
                     <hr className="text-bodyColor w-20 font-bold text-xl mx-5 " />
                     <h2 className='font-semibold text-xl lg:text-[35px] w-[400px]'>Our Partners</h2>
                 </span>
-                <div className="flex flex-wrap items-center justify-center lg:justify-between pt-10 gap-5 space-y-5">
+                <div className="flex flex-wrap items-center justify-center lg:justify-around pt-10 gap-10 space-y-5">
                     <Image className='w-[300px] h-full' src={marshellLogo} alt='logo' width={0} height={0} />
                     <Image className='w-[300px] h-full' src={odraLogo} alt='logo' width={0} height={0} />
                     <Image className='w-[300px] h-full' src={bortekLogo} alt='logo' width={0} height={0} />
