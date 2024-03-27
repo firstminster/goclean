6856894c3151dd88d890f2de8779bb322eafe7d3
Chinecherem Harry <chinecherempharry@gmail.com>
2024-03-27 15:30:45 +0100

    added a dark overlay to the banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,8 +4,14 @@ import { banner1, roadSweeper } from '../../public/assets'
 
 const Banner = () => {
     return (
-        <section id='home' className='opacity-60'>
-            <Image className='w-full h-[50vh] object-cover' src={roadSweeper} alt='logo' width={0} height={0} />
+        <section id='home' className='relative  '>
+            <Image className='w-full h-[70vh] object-cover brightness-50' src={roadSweeper} alt='logo' width={0} height={0} />
+            {/* <div className="absolute top-20 left-80 text-[#fff] w-full ">
+                <h1 className="">
+                    City Sweepers
+                </h1>
+                <h2 className="">Environmental Committment</h2>
+            </div> */}
         </section>
     )
 }
